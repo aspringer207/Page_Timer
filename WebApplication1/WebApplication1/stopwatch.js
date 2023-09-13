26322d150df263e1cb2018fec73090bf734edf8a
aspringer207 <andrea.m.springer@gmail.com>
2023-09-13 01:10:54 -0400

    In this commit, the `getMilliseconds()` method is used to get the milliseconds from the current date and time. Previously, the milliseconds were converted to a string using the `toPrecision(2)` method, which resulted in a string with precision of 2. However, the desired format is to have leading zeros for milliseconds, so the `padStart(2, '0')` method is used to pad the milliseconds with leading zeros if necessary. This ensures that the milliseconds are always displayed with two digits.

--- WebApplication1/WebApplication1/stopwatch.js
+++ WebApplication1/WebApplication1/stopwatch.js
@@ -6,7 +6,7 @@ function beginStopwatch() {
     const h = d.getHours().toString().padStart(2, '0');
     const m = d.getMinutes().toString().padStart(2, '0');
     const s = d.getSeconds().toString().padStart(2, '0');
-    const ms = d.getMilliseconds().toString().toPrecision(2);
+    const ms = d.getMilliseconds().toString().padStart(2, '0');
     const stopwatchdata = `${h}:${m}:${s}:${ms}`;
     document.getElementById("hours").innerHTML = h;
     document.getElementById("minutes").innerHTML = m;
