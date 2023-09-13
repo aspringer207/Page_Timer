75b3941db1a92e46be278f475bdb8f56ba5ac0e4
aspringer207 <andrea.m.springer@gmail.com>
2023-09-13 00:46:27 -0400

    inital

---
+++ WebApplication1/WebApplication1/stopwatch.js
@@ -0,0 +1,17 @@
+
+
+
+function beginStopwatch() {
+    const d = new Date();
+    const h = d.getHours().toString().padStart(2, '0');
+    const m = d.getMinutes().toString().padStart(2, '0');
+    const s = d.getSeconds().toString().padStart(2, '0');
+    const ms = d.getMilliseconds().toString().toPrecision(2);
+    const stopwatchdata = `${h}:${m}:${s}:${ms}`;
+    document.getElementById("hours").innerHTML = h;
+    document.getElementById("minutes").innerHTML = m;
+    document.getElementById("seconds").innerHTML = s;
+    document.getElementById("milliseconds").innerHTML = ms;
+    console.log('stopwatchdata');
+}
+    
---
+++ WebApplication1/WebApplication1/test.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    const d = new Date();
+    let msec = Date.parse;
+    let ds = d.toDateString();
+    console.log(msec);
+    document.getElementById("test").innerHTML = ds;
+    }
