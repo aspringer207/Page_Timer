7691f98a5d618f481f820b9e73d533040bc195bc
aspringer207 <andrea.m.springer@gmail.com>
2023-09-13 14:07:09 -0400

    clockloaderjs

---
+++ WebApplication1/WebApplication1/clockloadericons.js
@@ -0,0 +1,65 @@
+const clock_loader_icons =[circle, clock_loader_10, clock_loader_20, clock_loader_40, clock_loader_60, clock_loader_80, clock_loader_90, brightness_1]
+
+let circle1 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "circle",
+    element_class: "material-symbols-outlined",
+    code_point: "ef4a",
+}
+
+
+let clock_loader_10 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "clock_loader_10",
+    element_class: "material-symbols-outlined",
+    code_point: "f726",
+}
+let clock_loader_20 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "clock_loader_20",
+    element_class: "material-symbols-outlined",
+    code_point: "f725",
+}
+let clock_loader_40 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "clock_loader_40",
+    element_class: "material-symbols-outlined",
+    code_point: "f724",
+}
+let clock_loader_60 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "clock_loader_60",
+    element_class: "material-symbols-outlined",
+    code_point: "f723",
+}
+let clock_loader_80 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "clock_loader_80",
+    element_class: "material-symbols-outlined",
+    code_point: "f722",
+}
+let clock_loader_90 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "clock_loader_90",
+    element_class: "material-symbols-outlined",
+    code_point: "f721",
+}
+let brightness_1 = {
+    source: "Google Material Symbols",
+    type: "clock",
+    display_name: "brightness_1",
+    element_class: "material-symbols-outlined",
+    code_point: "e3a6",
+}
+for (i in clock_loader_icons) {
+    document.getElementById("stopwatchWidget");
+
+
+}
