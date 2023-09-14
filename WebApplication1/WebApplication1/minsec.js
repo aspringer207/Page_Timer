36de2a8c3e6bbb8e33769182a13eb89f7e188714
aspringer207 <andrea.m.springer@gmail.com>
2023-09-13 21:23:37 -0400

    add new

---
+++ WebApplication1/WebApplication1/minsec.js
@@ -0,0 +1,16 @@
+let text = "";
+const seconds = ["00"]
+
+
+for (let i = 0; i < 60; i++) {
+  text += i + "seconds <br>";
+  seconds.push(i.padStart(2, fillString))
+}
+console.log(text);
+document.write("demo.html");
+
+for (let i = 0; i < 60; i++) {
+    text += i + "seconds <br>";
+  }
+  console.log(text);
+  document.write("demo.html");
