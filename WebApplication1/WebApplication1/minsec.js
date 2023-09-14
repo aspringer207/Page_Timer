e54a453677501228998df65652897e290747b926
aspringer207 <andrea.m.springer@gmail.com>
2023-09-13 21:36:30 -0400

    tinker

--- WebApplication1/WebApplication1/minsec.js
+++ WebApplication1/WebApplication1/minsec.js
@@ -1,11 +1,12 @@
 let text = "";
 const seconds = ["00"]
 
-secstring = toString(i).padStart(2,"0")
+
 
 for (let i = 0; i < 60; i++) {
-  text += i + "seconds <br>";
-  seconds.push(secstring)
+    secstring = toString(i).padStart(2,"0")
+    text += i + "seconds <br>";
+    seconds.push(secstring);
    
 }
 console.log(text);
