39bdb0509432f7748d41cfda57ee5d66a8152449
aspringer207 <andrea.m.springer@gmail.com>
2023-09-13 21:46:52 -0400

    plus

--- WebApplication1/WebApplication1/minsec.js
+++ WebApplication1/WebApplication1/minsec.js
@@ -7,7 +7,6 @@ for (let i = 0; i < 60; i++) {
     secstring = toString(i).padStart(2,"0")
     text += i + "seconds <br>";
     seconds.push(secstring);
-   
 }
 console.log(text);
 document.getElementById("demo");
